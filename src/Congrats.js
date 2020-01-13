e63c12992c918da72e314ca07e76e2d828ec0376
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-01-13 16:22:13 +0400

    add Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -3,10 +3,15 @@ import React from 'react';
 /**
  * Functional react component for congratulatory message.
  * @function
+ * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if success prop is false)
  */
-export default () => {
-	return (
-		<div></div>
-	);
-}
+export default ({ success }) => (
+	<div data-test='component-congrats'>
+		{ success && (
+			<span data-test='congrats-message'>
+				Congratulations! You guessed the word!
+			</span>
+		) }
+	</div>
+);
