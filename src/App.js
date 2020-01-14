3f3b3d6724af50ccf915e32efeace6e71e74d770
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-01-14 19:09:11 +0400

    add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import React, { Component } from 'react';
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
 
-import './App.css';
-
 class App extends Component {
 	render() {
 		return (
-			<div data-test='component-app'>
+			<div data-test='component-app' className='container'>
         <h1>Jotto</h1>
 
 				<Congrats success={ true }/>
