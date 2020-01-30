3673ef9191f71d93cedc3cc2e873166773a22990
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2020-01-30 14:28:43 +0400

    add secretWord action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,11 @@
-import { getLetterMatchCount } from '../helpers';
+import axios from 'axios';
+
+import {getLetterMatchCount} from '../helpers';
 
 export const actionTypes = {
-	CORRECT_GUESS: 'CORRECT_GUESS',
-	GUESS_WORD: 'GUESS_WORD',
+    CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 /**
@@ -12,20 +15,28 @@ export const actionTypes = {
  * @returns {function} - Redux Thunk function.
  */
 export const guessWord = (guessedWord) => (dispatch, getState) => {
-	const secretWord = getState().secretWord;
-	const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
 
-	dispatch({
-		type: actionTypes.GUESS_WORD,
-		payload: {
-			guessedWord,
-			letterMatchCount,
-		},
-	});
+    dispatch({
+        type: actionTypes.GUESS_WORD,
+        payload: {
+            guessedWord,
+            letterMatchCount,
+        },
+    });
 
-	if(guessedWord === secretWord) {
-		dispatch({
-			type: actionTypes.CORRECT_GUESS,
-		});
-	}
+    if (guessedWord === secretWord) {
+        dispatch({
+            type: actionTypes.CORRECT_GUESS,
+        });
+    }
 };
+
+export const getSecretWord = () => (dispatch) => axios.get('http://localhost:3030')
+    .then((response) => {
+        dispatch({
+           type: actionTypes.SET_SECRET_WORD,
+           payload: response.data,
+        });
+    });
