291207658de6acfaabb085c17e7ac93166f487f8
Nikita Prytkov <prytkov.nikita.set@gmail.com>
2020-01-31 12:39:15 +0400

    connect app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import GuessedWords from './GuessedWords';
 import Congrats from './Congrats';
+import Input from './Input';
+import { getSecretWord } from './actions';
 
 class App extends Component {
 	render() {
+		const { success, guessedWords } = this.props;
+
 		return (
 			<div data-test='component-app' className='container'>
-        <h1>Jotto</h1>
+				<h1>Jotto</h1>
 
-				<Congrats success={ true }/>
+				<Congrats success={ success }/>
 
-        <GuessedWords guessedWords={[
-	        {
-	        	guessedWord: 'train',
-		        letterMatchCount: 3,
-	        },
-        ]}/>
+				<Input/>
+
+				<GuessedWords guessedWords={ guessedWords }/>
 			</div>
 		);
 	}
 }
 
-export default App;
+const mapStateToProps = ({ success, guessedWords, secretWord }) => ({
+	success,
+	guessedWords,
+	secretWord,
+});
+
+const mapDispatchToProps = {
+	getSecretWord,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/App.test.js
+++ src/App.test.js
@@ -3,21 +3,61 @@ import { shallow } from 'enzyme';
 
 import App from './App';
 
-import { findByTestAttr } from '../test/test-utils';
+import { findByTestAttr, storeFactory } from '../test/test-utils';
 
 /**
  * Factory function to create a ShallowWrapper for the App component.
  * @function setup
- * @param {object} props - Component props specific for this setup
+ * @param {object} initialState - Component props specific for this setup
  * @returns {ShallowWrapper}
  */
-const setup = (props = {}) => {
-  return shallow(<App { ...props }/>);
+const setup = (initialState = {}) => {
+	const store = storeFactory(initialState);
+
+	return shallow(<App store={ store }/>).dive().dive();
 };
 
 test('renders without error', () => {
-  const wrapper = setup();
-  const appComponent = findByTestAttr(wrapper, 'component-app');
+	const wrapper = setup();
+	const appComponent = findByTestAttr(wrapper, 'component-app');
 
-  expect(appComponent).toHaveLength(1);
+	expect(appComponent).toHaveLength(1);
+});
+
+describe('redux props', () => {
+	test('has "success" piece of state as prop', () => {
+		const success = false;
+		const wrapper = setup({ success });
+		const successProp = wrapper.instance().props.success;
+
+		expect(successProp).toBe(success);
+	});
+
+	test('has "guessedWords" piece of state as prop', () => {
+		const guessedWords = [
+			{
+				guessedWord: 'train',
+				letterMatchCount: 3,
+			},
+		];
+		const wrapper = setup({ guessedWords });
+		const guessedWordsProp = wrapper.instance().props.guessedWords;
+
+		expect(guessedWordsProp).toEqual(guessedWords);
+	});
+
+	test('has "secretWord" piece of state as prop', () => {
+		const secretWord = 'party';
+		const wrapper = setup({ secretWord });
+		const secretWordProp = wrapper.instance().props.secretWord;
+
+		expect(secretWordProp).toEqual(secretWord);
+	});
+
+	test('"getSecretWord" action creator as a function on the props', () => {
+		const wrapper = setup();
+		const getSecretWordProp = wrapper.instance().props.getSecretWord;
+
+		expect(getSecretWordProp).toBeInstanceOf(Function);
+	});
 });
